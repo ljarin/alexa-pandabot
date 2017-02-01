4993470aeced62a6baa56e1e389d3c78f477eab5
Laura Jarin-Lipschitz <ljarin@mit.edu>
2017-01-31 19:43:23 -0500

    update filepath

--- iot_2_gpio.py
+++ iot_2_gpio.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import shadow_pubsub
+import os.path
 
 # Requires certificate directory containing root-CA.crt, pandaDanceMotor.cert.pem, and pandaDanceMotor.private.key
+home=os.path.expanduser('~')
 endpoint='a2rig0317k0i1k.iot.us-east-1.amazonaws.com'
-rootCA='/home/ljarin/pandabot/certificate/root-CA.crt'
-cert='/home/ljarin/pandabot/certificate/pandaDanceMotor.cert.pem'
-key='/home/ljarin/pandabot/certificate/pandaDanceMotor.private.key'
+rootCA=home+'/pandabot/certificate/root-CA.crt'
+cert=home+'/pandabot/certificate/pandaDanceMotor.cert.pem'
+key=home+'/pandabot/certificate/pandaDanceMotor.private.key'
 
 
 myPandaBot=shadow_pubsub.initialize(endpoint,rootCA,cert,key)
